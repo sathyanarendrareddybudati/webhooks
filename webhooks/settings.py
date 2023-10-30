c3d8fba1173455425fd790bd5ee47d57f64dcf8a
sathyanarendrareddybudati <sathyanarendrareddybudati@gmail.com>
2023-10-30 10:42:09 +0530

    done

--- webhooks/settings.py
+++ webhooks/settings.py
@@ -141,22 +141,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL_FROM = os.getenv('EMAIL_TO')
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-# DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
-
-EMAIL_TO = 'gokulking40@gmail.com'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'satyabudati2000@gmail.com'
-EMAIL_HOST_PASSWORD = 'nano mkht zyiy morx'
-DEFAULT_FROM_EMAIL = 'Testing <satyabudati2000@gmail.com>'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 
 from corsheaders.defaults import default_headers
